13cb0a92cc2b698ebcc20d0b864b061d00a7480e
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-11-05 14:39:36 +0600

    books data fetched

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,11 @@
+import Banner from "../Banner/Banner";
+import Books from "../Books/Books";
+
 const Home = () => {
     return (
         <div>
-            <h2>THis is a Home Page</h2>
+            <Banner></Banner>
+            <Books></Books>
         </div>
     );
 };
